aba92665b73fa4a5630b739aa35e1c7ba6cc0e7c
austine616 <en21aai@herts.ac.uk>
2025-12-10 15:44:48 +0000

    changing values for my graphs
    Updated histogram breaks from 10 to 20 for better visualization.

--- script.R
+++ script.R
@@ -1,13 +1,21 @@
 
-plot( carbon_segment$Population, carbon_segment$CO2emissionspercapita,
-      type = "l", xlab = "Global Population", 
-      ylab = "CO₂ Emissions Per Capita (metric tons per person)",
-      main = "Relationship Between Global Population and CO₂ Emissions Per Capita (1983–2008)" )
+plot(
+  carbon_segment$Population,
+  carbon_segment$CO2emissionspercapita,
+  type = "l",
+  xlab = "Global Population",
+  ylab = "CO2 Emissions Per Capita (metric tons per person)",
+  main = "Relationship Between Global Population and CO2 Emissions Per Capita (1983–2008)"
+)
 
 
-hist( carbon_segment$CO2emissionspercapita, 
-      breaks = 10, 
-      col = "lightblue",
-      xlab = "CO₂ Emissions Per Capita", 
-      main = "Distribution of CO₂ Emissions Per Capita (1983–2008)" )
+hist(
+  carbon_segment$CO2emissionspercapita,
+  breaks = 20,
+  col = "lightblue",
+  xlab = "CO2 Emissions Per Capita",
+  main = "Distribution of CO2 Emissions Per Capita (1983–2008)"
+)
+
+
 
